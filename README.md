c5734f196b1084c82040cf0e27056cde5871f3bd
CatRass <catrassyt@gmail.com>
2021-08-13 12:45:42 +1000

    Fixed false extension (.css to .cs)

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 # Bacchus Marsh Grammar Discord Server Bot
 This is a Discord Bot created by Tristen L. and Darrel M.
 The bot is designed to automate several facets of the server, like:
-⋅⋅* Setting Reminders for upcoming SAC's and Events
-⋅⋅* Self-role functionality through reactions
+* Setting Reminders for upcoming SAC's and Events
+* Self-role functionality through reactions
---
+++ TestProg.cs
@@ -0,0 +1,50 @@
+<<<<<<< HEAD
+public class Program
+{
+	private DiscordSocketClient _client;
+
+	public static void Main(string[] args)
+		=> new Program().MainAsync().GetAwaiter().GetResult();
+
+	public async Task MainAsync()
+	{
+		_client = new DiscordSocketClient();
+		_client.Log += Log;
+		await _client.LoginAsync(TokenType.Bot,
+			Environment.GetEnvironmentVariable("DiscordToken"));
+		await _client.StartAsync();
+
+		// Block this task until the program is closed.
+		await Task.Delay(-1);
+	}
+	private Task Log(LogMessage msg)
+	{
+		Console.WriteLine(msg.ToString());
+		return Task.CompletedTask;
+	}
+=======
+public class Program
+{
+	private DiscordSocketClient _client;
+
+	public static void Main(string[] args)
+		=> new Program().MainAsync().GetAwaiter().GetResult();
+
+	public async Task MainAsync()
+	{
+		_client = new DiscordSocketClient();
+		_client.Log += Log;
+		await _client.LoginAsync(TokenType.Bot,
+			Environment.GetEnvironmentVariable("DiscordToken"));
+		await _client.StartAsync();
+
+		// Block this task until the program is closed.
+		await Task.Delay(-1);
+	}
+	private Task Log(LogMessage msg)
+	{
+		Console.WriteLine(msg.ToString());
+		return Task.CompletedTask;
+	}
+>>>>>>> b169a29e1607c8f61315e5128a0751492c3c8879
+}
