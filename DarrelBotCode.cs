1f1815ec55e42ed78bb78605ab59fd5220dbed4f
CatRass <catrassyt@gmail.com>
2021-08-19 18:57:11 +1000

    Commands Added
    - Added prefix '!'
    - Added default Command "hello"
    - Created Command Handler

--- DarrelBotCode.cs
+++ DarrelBotCode.cs
@@ -8,39 +8,68 @@ namespace BMGBotLocalCopy
 {
 	public class Program
 	{
-        private DiscordSocketClient _client;
+		private DiscordSocketClient _client;
 
-        public static void Main(string[] args)
+		public static void Main(string[] args)
 			=> new Program().MainAsync().GetAwaiter().GetResult();
 
 		public async Task MainAsync()
 		{
 			_client = new DiscordSocketClient();
-
+			_client.MessageReceived += CommandHandler;
 			_client.Log += Log;
 
-			//  You can assign your bot token to a string, and pass that in to connect.
-			//  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-
-            // NOTE: token go here :>
-			var token = "token";
+			var token = "Token here :D";
 
 			// Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-			// var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
 			// var token = File.ReadAllText("token.txt");
-			// var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
 
 			await _client.LoginAsync(TokenType.Bot, token);
 			await _client.StartAsync();
 
-			// Block this task until the program is closed.
 			await Task.Delay(-1);
 		}
 
-        private Task Log(LogMessage msg)
-        {
+		private Task Log(LogMessage msg)
+		{
 			Console.WriteLine(msg.ToString());
 			return Task.CompletedTask;
 		}
-    }
+
+		private Task CommandHandler(SocketMessage message)
+		{
+
+			// Removes Prefix for processing
+			string command = "";
+			int lengthOfCommand = -1;
+
+			// Prefix Filter
+			if (!message.Content.StartsWith('!'))           // PREFIX : CHANGE LATER
+				return Task.CompletedTask;
+
+			// Ignores Commands from Bots
+			if (message.Author.IsBot)
+				return Task.CompletedTask;
+
+
+			if (message.Content.Contains(" "))
+				lengthOfCommand = message.Content.IndexOf(" ");
+			else
+				lengthOfCommand = message.Content.Length;
+			command = message.Content.Substring(1, lengthOfCommand - 1);
+
+
+			// Test command, "!hello"
+			if (command.Equals("hello"))
+			{
+				message.Channel.SendMessageAsync($@"Hi {message.Author.Mention}, I'm BMG Bot! Go shit yourself!");
+			}
+			else if (command.Equals("links"))
+            {
+
+            }
+
+				return Task.CompletedTask;
+		}
+	}
 }
